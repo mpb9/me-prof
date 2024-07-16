f2ce19d0edc0539cd1f967279e3fb98475ca13f9
mpb9 <michaelbeebe1031@gmail.com>
2024-07-16 08:17:38 -0400

    removed unneeded code in App.js changeTheme()

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { useState } from "react";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
-import { Dark, GreyScale, Light } from "./data/Themes";
+import { Dark } from "./data/Themes";
 
 export default function App() {
   const [theme, setTheme] = useState(Dark);
 
   const changeTheme = (new_theme) => {
-    if (new_theme === Dark) {
-      setTheme(Dark);
-    } else if (new_theme === Light) {
-      setTheme(Light);
-    } else if (new_theme === GreyScale) {
-      setTheme(GreyScale);
-    }
+    setTheme(new_theme);
   };
 
   return (
