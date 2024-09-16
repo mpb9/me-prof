bceb6627d538e06afcfd90e4fcf9f1ad94c677e3
mpb9 <michaelbeebe1031@gmail.com>
2024-09-16 14:43:45 -0400

    theater entrance upgrades

--- src/components/theater/TheaterEntrance.js
+++ src/components/theater/TheaterEntrance.js
@@ -6,15 +6,15 @@ export default function TheaterEntrance() {
       <div className='bg-base-100 bg-grid-2-s-1-[#A5B4FC44] brick-wall-border flex h-2/5 w-screen border-x-[2.75rem]'>
         {/* Left Wall */}
         <div
-          className='h-min-full from-base-200 to-base-100 border-base-200 w-[28%] border-l-[12px] bg-gradient-to-tr'
+          className='h-min-full from-base-200 to-base-100 border-base-200 w-[26%] border-l-[32px] bg-gradient-to-tr'
           style={{ clipPath: 'polygon(18% 0, 100% 10%, 100% 100%, 0 100%, 0 0)' }}></div>
         {/* Entrance */}
-        <div className='inline-flex h-full w-[44%] items-end'>
+        <div className='inline-flex h-full w-[48%] items-end'>
           <div className='bg-base-100 w-full' style={{ height: '90%' }}></div>
         </div>
         {/* Right Wall */}
         <div
-          className='from-base-200 to-base-100 border-base-200 h-full w-[28%] border-r-[12px] bg-gradient-to-tl'
+          className='from-base-200 to-base-100 border-base-200 h-full w-[26%] border-r-[32px] bg-gradient-to-tl'
           style={{ clipPath: 'polygon(0 10%, 100% 0, 100% 100%, 0 100%, 0 82%)' }}></div>
       </div>
 
