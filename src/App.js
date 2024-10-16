788d3fe2edc3c079385e56eaae56e8f3982bc983
mpb9 <michaelbeebe1031@gmail.com>
2024-10-16 11:32:51 -0400

    added routes to dev, stuff, and new-tab

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import CoolStuff from './components/cool_stuff/CoolStuff';
+import NewTab from './components/cool_stuff/new_tab/NewTab';
+import DevWork from './components/dev_work/DevWork';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Theater from './components/theater/Theater';
@@ -15,7 +19,15 @@ export default function App() {
   if (useTheaterTheme) {
     return (
       <div className='min-h-screen'>
-        <Theater />
+        <Router>
+          <Routes>
+            <Route path='/' element={<Theater />} />
+            <Route path='/dev' element={<DevWork />} />
+            <Route path='/stuff' element={<CoolStuff />} />
+            <Route path='/new-tab' element={<NewTab />} />
+            <Route path='*' element={<Navigate to='/' />} />
+          </Routes>
+        </Router>
       </div>
     );
   }
