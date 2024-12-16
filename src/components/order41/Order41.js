800eb307326feec8ed3c637b4039328218bf1b80
mpb9 <michaelbeebe1031@gmail.com>
2024-12-15 22:06:43 -0500

    Add new font faces and PlayerScore component; refactor Order41 layout

--- src/components/order41/Order41.js
+++ src/components/order41/Order41.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import {
-  // connerId,
-  // edelId,
+  connerId,
+  edelId,
   fetchLeagueData,
   fetchMatchupsData,
   fetchRostersData,
@@ -9,9 +9,9 @@ import {
   leagueUrl,
   mikeId,
   pechId,
-  // shimId,
+  shimId,
   slopId,
-} from './SleeperService';
+} from './services/SleeperService';
 import TeamScore from './TeamScore';
 export default function Order41() {
   const [error, setError] = useState(null);
@@ -21,6 +21,7 @@ export default function Order41() {
   const [users, setUsers] = useState(null);
   const [lastUpdated, setLastUpdated] = useState(null);
   const refreshRate = 10 * 1000;
+  const sColor = '#AED998';
 
   // MARK: useEffects
   useEffect(() => {
@@ -99,22 +100,23 @@ export default function Order41() {
 
   // MARK: Render
   return (
-    <div className='relative w-full min-h-screen my-font-monospace bg-[radial-gradient(circle_700px_at_50%_400px,#3E3E3E,transparent)]'>
+    <div className='relative w-full my-font-monospace bg-[radial-gradient(circle_700px_at_50%_400px,#3E3E3E,transparent)]'>
       {/* HEADER */}
-      <div className='flex items-center justify-center text-stone-950 w-full text-center bg-stone-400 h-[4rem] px-0 sm:px-6 md:px-12'>
-        <div className='hidden w-1/4 text-left sm:block'>
-          <p className='text-sm'>
-            LAST UPDATE
-            <br /> <b className='text-base'>{lastUpdated?.toLocaleTimeString()}</b>
+      <div className='flex items-center justify-center text-stone-950 w-full text-center sm:bg-[#948c87] h-[4rem] px-0 sm:px-6 md:px-8'>
+        <div className='hidden w-1/4 text-left cursor-default sm:block'>
+          <p className='text-xl md:text-2xl my-font-terminess w-fit px-2 py-1 lg:py-[6px] border-[1px] rounded-lg border-[#AED998] bg-stone-900 text-[#AED998]'>
+            {lastUpdated?.toLocaleTimeString()}
           </p>
         </div>
-        <h1 className='w-1/2 text-[3rem] font-bold my-font-electro hidden sm:block md:text-[3.75rem]'>Order 41</h1>
+        <h1 className='w-1/2 text-[3rem] font-bold my-font-electro hidden cursor-default sm:block md:text-[3.5rem] lg:text-[3.75rem]'>
+          Order 41
+        </h1>
         <div className='flex items-center justify-center w-full h-full sm:justify-end sm:w-1/4'>
           <a
             href={leagueUrl}
             target='_blank'
             rel='noreferrer'
-            className='w-[2.75rem] h-full py-[9px] hover:scale-110 transition-transform'>
+            className='w-[2.75rem] h-full py-[6px] hover:scale-110 transition-transform'>
             <div className='w-full h-full bg-no-repeat bg-contain bg-sleeper-green' />
           </a>
         </div>
@@ -122,11 +124,12 @@ export default function Order41() {
 
       {/* MATCHUPS */}
       {recievedData() && matchups !== null ? (
-        <div className='flex flex-col items-center justify-start w-full h-[calc(100vh-4rem)]'>
-          <div className='w-[95%] px-4 z-10 h-full'>
-            <h1 className='pb-3 pt-5 text-3xl font-bold text-center cursor-default text-[#aed998]'>ROUND ONE</h1>
+        <div className='flex flex-col items-center justify-start w-full h-[calc(100vh-4rem)] overflow-scroll'>
+          <div className='w-[95%] px-4 h-full'>
+            <h1 className='pb-2 pt-2 sm:pt-5 text-3xl font-bold text-center cursor-default text-[#aed998]'>
+              ROUND ONE
+            </h1>
             {/* 3 WAY MATCHUP */}
-            {/* <div className='grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 h-[calc(100%-6rem)]'> */}
             <div className='grid grid-cols-1 gap-2 pb-8 overflow-scroll sm:grid-cols-2 md:grid-cols-3'>
               <TeamScore
                 key={pechId}
@@ -145,6 +148,28 @@ export default function Order41() {
                 user={getUserFromUserId(slopId)}
                 roster={getRosterFromUserId(slopId)}
                 matchup={getMatchupFromUserId(slopId)}
+              />
+            </div>
+            {/* BYE */}
+            <h1 className='pb-2 text-2xl italic text-center cursor-default text-[#aed998]'>ON BYE</h1>
+            <div className='grid grid-cols-1 gap-2 pb-8 overflow-scroll sm:grid-cols-2 md:grid-cols-3'>
+              <TeamScore
+                key={edelId}
+                user={getUserFromUserId(edelId)}
+                roster={getRosterFromUserId(edelId)}
+                matchup={getMatchupFromUserId(edelId)}
+              />
+              <TeamScore
+                key={shimId}
+                user={getUserFromUserId(shimId)}
+                roster={getRosterFromUserId(shimId)}
+                matchup={getMatchupFromUserId(shimId)}
+              />
+              <TeamScore
+                key={connerId}
+                user={getUserFromUserId(connerId)}
+                roster={getRosterFromUserId(connerId)}
+                matchup={getMatchupFromUserId(connerId)}
               />
             </div>
           </div>
--- src/components/order41/TeamScore.js
+++ src/components/order41/TeamScore.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
-import { generatePlayer } from './PlayerService';
+import PlayerScore from './PlayerScore';
+import { fetchPlayersByLineupStatus, fetchTeamData } from './services/PlayerService';
 
 TeamScore.propTypes = {
   user: PropTypes.object.isRequired,
@@ -10,35 +11,35 @@ TeamScore.propTypes = {
 
 export default function TeamScore(props) {
   const [error, setError] = useState(null);
-  const [players, setPlayers] = useState(null);
-  const [showBench, setShowBench] = useState(true);
-  const [starterIds, setStarterIds] = useState(props.matchup.starters);
-  const [startersPoints, setStartersPoints] = useState(props.matchup.starters_points);
-  const [playerPoints, setPlayersPoints] = useState(props.matchup.players_points);
-  const [playerIds, setPlayerIds] = useState(props.matchup.players);
+  const [showBench, setShowBench] = useState(false);
+  const [team, setTeam] = useState(null);
+  const [starters, setStarters] = useState(null);
+  const [bench, setBench] = useState(null);
+  const [taxi, setTaxi] = useState(null);
+  const [ir, setIr] = useState(null);
 
   useEffect(() => {
     const getPlayers = async () => {
       try {
-        const data = await generatePlayer(
+        const teamData = await fetchTeamData(
           props.matchup.players,
           props.matchup.players_points,
           props.matchup.starters,
           props.roster.reserve,
           props.roster.taxi
         );
-        setPlayers(data);
-        setError(null);
-        // if (starterIds !== props.matchup.starters) setStarterIds(props.matchup.starters);
-        // if (startersPoints !== props.matchup.starters_points) setStartersPoints(props.matchup.starters_points);
-        // if (playerPoints !== props.matchup.players_points) setPlayersPoints(props.matchup.players_points);
-        // if (playerIds !== props.matchup.players) setPlayerIds(props.matchup.players);
+        const starterData = fetchPlayersByLineupStatus(teamData, 'starter');
+        const benchData = fetchPlayersByLineupStatus(teamData, 'bench');
+        const taxiData = fetchPlayersByLineupStatus(teamData, 'taxi');
+        const reserveData = fetchPlayersByLineupStatus(teamData, 'ir');
 
-        // if (props.user.user_id === '465557925683785728') {
-        //   console.log(new Date().toLocaleTimeString());
-        //   console.log('mike starterIds:', starterIds);
-        //   console.log('mike startersPoints:', props.matchup.starters_points);
-        // }
+        setTeam(teamData);
+        setStarters(starterData);
+        setBench(benchData);
+        setTaxi(taxiData);
+        setIr(reserveData);
+
+        setError(null);
       } catch (err) {
         setError(err.message);
       }
@@ -46,23 +47,8 @@ export default function TeamScore(props) {
     getPlayers();
   }, [props]);
 
-  const getStarters = () => {
-    if (recievedData()) return players.filter((player) => player[3] === 'starter');
-  };
-  const getBench = () => {
-    if (recievedData()) return players.filter((player) => player[3] === 'bench');
-  };
-  const getTaxi = () => {
-    if (recievedData()) return players.filter((player) => player[3] === 'taxi');
-  };
-  const getReserve = () => {
-    if (recievedData()) return players.filter((player) => player[3] === 'ir');
-  };
-
   const recievedData = () => {
-    return (
-      players !== null && starterIds !== null && startersPoints !== null && playerPoints !== null && playerIds !== null
-    );
+    return team !== null && starters !== null && bench !== null && taxi !== null && ir !== null;
   };
 
   const toggleBench = () => {
@@ -72,32 +58,33 @@ export default function TeamScore(props) {
   return (
     <div className='w-full h-full overflow-scroll text-center text-black'>
       {/* NAME */}
-      <div className='flex flex-col items-center justify-center w-full px-2 py-3 h-[4.75rem] overflow-hidden border-2 rounded-t-lg border-stone-600 min-h-[4.75rem] bg-stone-900 lg:h-fill lg:min-h-0 lg:py-6'>
-        <span className='text-lg font-black leading-5 text-stone-300'>{props.user.team_name}</span>
-        <span className='pt-2 text-sm italic text-stone-400'>
+      <div className='flex flex-col items-center justify-center w-full px-2 py-3 h-[4.5rem] overflow-hidden border-2 rounded-t-lg border-stone-600 min-h-[4.5rem] bg-stone-900 lg:h-fill lg:min-h-0 lg:py-6'>
+        <span className='text-base font-black leading-5 lg:text-lg text-stone-300'>{props.user.team_name}</span>
+        <span className='pt-1 text-sm italic text-stone-400 lg:pt-0'>
           {props.roster.settings.wins}-{props.roster.settings.losses}
         </span>
       </div>
 
       {/* PTS */}
-      <div className='w-full py-2 bg-stone-950 border-x-2 border-stone-600'>
-        <span className='text-lg font-bold leading-6 text-[#aed998]'>{props.matchup.points} pts</span>
+      <div className='w-full py-1 bg-stone-950 border-x-2 border-stone-600'>
+        <span className='text-lg font-bold text-[#aed998]'>{props.matchup.points} pts</span>
       </div>
 
       {/* ROSTER */}
-      {players !== null ? (
+      {recievedData() ? (
         <div className='w-full rounded-b-lg'>
-          {getStarters().map((player, index) => (
-            <div
-              key={player[0]}
-              className='flex bg-stone-400 items-center overflow-hidden justify-center w-full h-8 text-base border-2 border-b-0 border-stone-600 sm:text-[0.85rem] lg:text-base'>
-              <span className='w-3/4 pl-4 text-left text-stone-900 whitespace-nowrap'>{player[1]}</span>
-              <span className='w-1/4 h-full py-1 border-l-2 bg-stone-950 text-stone-200 border-stone-600'>
-                {player[2]}
-              </span>
-            </div>
+          {/* STARTERS */}
+          {starters.map((player, index) => (
+            <PlayerScore
+              key={index}
+              id={player[0]}
+              first_name={player[3].first_name}
+              last_name={player[3].last_name}
+              pts={player[1]}
+              pos={player[3].position}
+              team={player[3].team}
+            />
           ))}
-
           <div
             className='flex items-center justify-center w-full h-8 border-2 border-b-0 cursor-pointer bg-stone-900 border-stone-600 hover:bg-stone-600 text-stone-200'
             onClick={toggleBench()}>
@@ -106,35 +93,44 @@ export default function TeamScore(props) {
           {/* BENCH */}
           {showBench ? (
             <div className='bg-stone-400 sm:text-[0.85rem] lg:text-base text-base w-full border-b-2 border-stone-600'>
-              {getBench().map((player, index) => (
-                <div
+              {bench.map((player, index) => (
+                <PlayerScore
                   key={index}
-                  className='flex items-center justify-between w-full h-8 border-2 border-b-0 border-stone-600'>
-                  <span className='w-3/4 pl-4 overflow-hidden text-left text-stone-900'>{player[1]}</span>
-                  <span className='w-1/4 h-full bg-stone-950 text-stone-200'>{player[2]}</span>
-                </div>
+                  id={player[0]}
+                  first_name={player[3].first_name}
+                  last_name={player[3].last_name}
+                  pts={player[1]}
+                  pos={player[3].position}
+                  team={player[3].team}
+                />
               ))}
               <div className='flex items-center justify-center w-full h-8 border-2 border-b-0 bg-stone-900 border-stone-600 text-stone-200'>
                 <span className='tracking-[0.15rem]'>IR</span>
               </div>
-              {getReserve().map((player, index) => (
-                <div
+              {ir.map((player, index) => (
+                <PlayerScore
                   key={index}
-                  className='flex items-center justify-between w-full h-8 border-2 border-b-0 border-stone-600'>
-                  <span className='w-3/4 pl-4 overflow-hidden text-left text-stone-900'>{player[1]}</span>
-                  <span className='w-1/4 h-full bg-stone-950 text-stone-200'>{player[2]}</span>
-                </div>
+                  id={player[0]}
+                  first_name={player[3].first_name}
+                  last_name={player[3].last_name}
+                  pts={player[1]}
+                  pos={player[3].position}
+                  team={player[3].team}
+                />
               ))}
               <div className='flex items-center justify-center w-full h-8 border-2 border-b-0 bg-stone-900 border-stone-600 text-stone-200'>
                 <span className='tracking-[0.15rem]'>Taxi</span>
               </div>
-              {getTaxi().map((player, index) => (
-                <div
+              {taxi.map((player, index) => (
+                <PlayerScore
                   key={index}
-                  className='flex items-center justify-between w-full h-8 border-2 border-b-0 border-stone-600'>
-                  <span className='w-3/4 pl-4 overflow-hidden text-left text-stone-900'>{player[1]}</span>
-                  <span className='w-1/4 h-full bg-stone-950 text-stone-200'>{player[2]}</span>
-                </div>
+                  id={player[0]}
+                  first_name={player[3].first_name}
+                  last_name={player[3].last_name}
+                  pts={player[1]}
+                  pos={player[3].position}
+                  team={player[3].team}
+                />
               ))}
             </div>
           ) : (
